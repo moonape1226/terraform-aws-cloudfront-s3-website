9344adefd0338170e7a825a70dfb89f29c25ce5d
Carl Tsai <carltsai@askmiso.com>
2023-10-25 10:57:56 +0800

    Add output cloudfront_dist_hosted_zone_id

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "cloudfront_dist_id" {
   value = aws_cloudfront_distribution.s3_distribution.id
 }
 
+output "cloudfront_dist_hosted_zone_id" {
+  value = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+}
+
 output "s3_domain_name" {
   value = aws_s3_bucket_website_configuration.bucket-website.website_domain
 }
